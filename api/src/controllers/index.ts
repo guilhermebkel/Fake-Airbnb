90b401467cd78481ea952ea5419e7f786b1b7c48
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-03 22:55:11 -0300

    Add upload module and finish controllers

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,9 +1,21 @@
 import { Express, Request, Response } from 'express'
+import * as multer from 'multer'
+
+import Upload from '../modules/Upload'
+
 import User from '../models/User'
+import Spot from '../models/Spot'
+import Booking from '../models/Booking'
+
+const upload = multer(Upload)
 
 export default {
   config(app: Express){
     app.post('/users', createUser)
+    app.post('/spots', upload.single('thumbnail'), createSpot)
+    app.get('/spots', getSpots)
+    app.get('/dashboard', getUserSpots)
+    app.post('/spots/:spot_id/bookings', createBooking)
   }
 }
 
@@ -19,6 +31,82 @@ async function createUser(req: Request, res: Response){
     }
 
     return res.json({ success: true, data: user })
+  }
+  catch(error){
+    console.error(error)
+    return res.json({ success: false, error })
+  }
+}
+
+async function createSpot(req: any, res: Response){
+  try{
+    const { filename } = req.file
+    const { company, techs, price } = req.body
+    const { user_id } = req.headers
+
+    const user = await User.findById(user_id)
+
+    if (!user) return res.json({ success: false, error: 'User does not exist!' })
+
+    const spot = await Spot.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      techs: techs.split(',').map(tech => tech.trim()),
+      price
+    })
+
+    return res.json({ success: true, data: spot })
+  }
+  catch(error){
+    console.error(error)
+    return res.json({ success: false, error })
+  }
+}
+
+async function getSpots(req: Request, res: Response){
+  try{
+    const { tech } = req.query
+    
+    const spots = await Spot.find({ techs: tech })
+
+    return res.json({ success: true, data: spots })
+  }
+  catch(error){
+    console.error(error)
+    return res.json({ success: false, error })
+  }
+}
+
+async function getUserSpots(req: Request, res: Response){
+  try{
+    const { user_id } = req.headers
+
+    const spots = await Spot.find({ user: user_id })
+
+    return res.json({ success: true, data: spots })
+  }
+  catch(error){
+    console.error(error)
+    return res.json({ success: false, error })
+  }
+}
+
+async function createBooking(req: Request, res: Response){
+  try{
+    const { user_id } = req.headers
+    const { spot_id } = req.params
+    const { date } = req.body
+
+    const booking = await Booking.create({
+      user: user_id,
+      spot: spot_id,
+      date,
+    })
+
+    await booking.populate('spot').populate('user').execPopulate()
+
+    return res.json({ success: true, data: booking })
   }
   catch(error){
     console.error(error)
---
+++ api/src/models/Booking.ts
@@ -0,0 +1,17 @@
+import * as mongoose from 'mongoose'
+
+const Booking = new mongoose.Schema({
+  date: String,
+  approved: Boolean,
+  price: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  spot: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Spot'
+  }
+})
+
+export default mongoose.model('Booking', Booking)
