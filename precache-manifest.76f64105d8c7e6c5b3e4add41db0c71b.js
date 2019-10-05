ed727d6e97ed70647c18218931a2184d59f196c4
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-04 23:34:21 -0300

    Updates

---
+++ precache-manifest.76f64105d8c7e6c5b3e4add41db0c71b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d654b2a1e62a20faf1a496d832e15881",
+    "url": "/Fake-Airbnb/index.html"
+  },
+  {
+    "revision": "b5b01c7eab0e65557cc4",
+    "url": "/Fake-Airbnb/static/css/main.f20d831a.chunk.css"
+  },
+  {
+    "revision": "b59b471f6ec7df4a2ddc",
+    "url": "/Fake-Airbnb/static/js/2.ce47b4cb.chunk.js"
+  },
+  {
+    "revision": "b5b01c7eab0e65557cc4",
+    "url": "/Fake-Airbnb/static/js/main.ad943e41.chunk.js"
+  },
+  {
+    "revision": "a5119f35550f3e64d7b9",
+    "url": "/Fake-Airbnb/static/js/runtime-main.9ed54be0.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/Fake-Airbnb/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/Fake-Airbnb/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "740528028131fdef515dcb930373ccf6",
+    "url": "/Fake-Airbnb/static/media/logo.74052802.svg"
+  }
+]);
