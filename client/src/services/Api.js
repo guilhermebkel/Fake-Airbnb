c8308aa7beaa374b2572e388245a06247a9abe13
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-04 23:41:12 -0300

    Change url

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://fakeairbnb.herokuapp.com'
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : 'https://fakeairbnbapp.herokuapp.com'
 })
 
 export default Api
