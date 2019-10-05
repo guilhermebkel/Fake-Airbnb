f7990ca255134e2bdc5828ec08f82b0a46ea17a3
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-04 21:52:35 -0300

    Add login css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import './App.css'
 
+import Logo from './assets/logo.svg'
+
 function App() {
   return (
-    <h1>
-      TESTE
-    </h1>
+    <div className="container">
+      <img src={Logo} alt="" />
+
+      <div className="content">
+        <p>
+          Show <strong>spots</strong> for developers
+          and find <strong>talents</strong> for your brand.
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Type your email..."
+          />
+          <button stype="submit" className="btn">Login</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
